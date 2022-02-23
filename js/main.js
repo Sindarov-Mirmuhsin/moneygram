9a467b475ca66c684a8a0f53f5fcf3161d76ed2c
Sindarov-Mirmuhsin <therealgreen416@gmail.com>
2022-02-23 22:01:13 +0500

    Initail commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var elForm = document.querySelector(".form");
+var elInput = document.querySelector(".form__input");
+var elSelect = document.querySelector(".form__select");
+var elResult = document.querySelector(".form__result");
+var elBtn = document.querySelector(".form__btn");
+
+var uzs = 10851.69;
+var rubl = 79.61;
+var evro = 0.88;
+
+elForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  var elInputVal = Number(elInput.value);
+  var elSelectVal = Number(elSelect.value);
+  var elResultVal = elInputVal * elSelectVal;
+  
+  
+  
+  if (elInputVal > 0) {
+    elResult.textContent = (`${elResultVal}`);
+  } else if (elInputVal < 0) {
+    elResult.textContent = ("Iltimos 0 dan katta qiymat kiriting");
+  } else if (elInputVal = "") {
+    elResult.textContent = ("Qiymat topilmadi Iltimos pul miqdorini kiriting");
+  } else if (elInputVal != Number) {
+    elResult.textContent = ("Iltimos pul miqdorini raqam ko'rinishida kiriting");
+  }
+})
